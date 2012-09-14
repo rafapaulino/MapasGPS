f69a68b645f9214c420050665cabf3f92e9e1a8c
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-14 11:39:05 -0300

    Initial Commit

---
+++ MapasGPSTests/MapasGPSTests.m
@@ -0,0 +1,32 @@
+//
+//  MapasGPSTests.m
+//  MapasGPSTests
+//
+//  Created by Rafael Brigagão Paulino on 14/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "MapasGPSTests.h"
+
+@implementation MapasGPSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MapasGPSTests");
+}
+
+@end
